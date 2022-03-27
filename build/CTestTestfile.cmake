35ee36242f765ef28bcf28772ec68ad9c4b808ec
ElcarimQAQ <1067095308@qq.com>
2022-03-27 21:20:14 +0800

    first add

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/bingda/demo_ms/src
+# Build directory: /home/bingda/demo_ms/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hello")
---
+++ build/hello/CMakeFiles/hellohello_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellohello_c.dir/src/hello_c.cpp.o"
+  "/home/bingda/demo_ms/devel/lib/hello/hellohello_c.pdb"
+  "/home/bingda/demo_ms/devel/lib/hello/hellohello_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellohello_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
