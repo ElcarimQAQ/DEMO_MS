96336add31d46e809d457cba71bc28cd5035de8f
ElcarimQAQ <1067095308@qq.com>
2022-04-06 18:16:07 +0800

    add pub

--- src/pub_sub/src/pub01.cpp
+++ src/pub_sub/src/pub01.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
@@ -7,10 +8,18 @@ int main(int argc, char *argv[])
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<std_msgs::String>("fang",10);
     std_msgs::String msg;
+    ros::Rate rate(10);
+    int count = 0;
     while (ros::ok)
     {
-        msg.data = "hello";
+        count++;
+        std::stringstream ss;
+        ss << "hello ---" << count;
+        //msg.data = "hello";
+        msg.data = ss.str();
         pub.publish(msg);
+
+        rate.sleep();
     }
     
     return 0;
